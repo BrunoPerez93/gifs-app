af904f49299d988bbb75b8dbc4d77b19c4a98415
Bruno Perez <brunoperez24793@gmail.com>
2024-08-01 18:17:11 -0300

    fix images

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -5,8 +5,7 @@ export const GifItem = ({ title, url}) => {
 
   return (
     <div className="card">
-        <img src={ url } alt={ title } />
-        <p>{ title }</p>
+        <img src={ url } alt={ title } width={300} height={200} />
     </div>
   )
 }
